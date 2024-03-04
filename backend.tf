59ec73fa71967c1b20bf6637728b0b5b5a9b9919
ashikjen200 <35518899+ashikjen200@users.noreply.github.com>
2024-03-04 08:36:55 +0530

    Update dynamodb locking

--- backend.tf
+++ backend.tf
@@ -3,5 +3,6 @@ terraform {
     bucket         = "myterraform-backend-poc"
     key            = "terraform.tfstate"
     region         = "ap-south-1"
+    dynamodb_table = "tfe-locking-table"
   }
 }
